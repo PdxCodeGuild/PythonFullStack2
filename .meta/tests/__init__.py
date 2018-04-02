2ba34908860b82fc7ae3526deb0ed148398fdc0d
Zack Kollar (SeedyROM) <rallokkcaz@gmail.com>
2018-04-02 12:12:14 -0700

    Added new coverage stats

--- .meta/tests/__init__.py
+++ .meta/tests/__init__.py
@@ -3,6 +3,9 @@ import time
 import sys
 from functools import reduce
 from collections import Counter
+
+import anybadge
+
 from colorama import Fore, Style
 from tests.helpers import markdown, BASE_DIR
 
@@ -12,11 +15,29 @@ def check_readme():
         files = markdown.get_completed_list_items(lists)
         return files
 
+def generate_coverage_badge(coverage):
+    badge_path = os.path.join(BASE_DIR, '.meta', 'media', 'coverage.svg')
+    badge = anybadge.Badge(
+        'coverage', 
+        coverage, 
+        thresholds = {
+            50: 'red' ,
+            60: 'orange', 
+            80: 'yellow',
+            100: 'green'
+        },
+        value_suffix='%'
+    )
+    os.remove(badge_path)
+    badge.write_badge(badge_path)
+
 def generate_output(verbose=False):
+    # Measure test time
     t = time.perf_counter()
     files = check_readme()
     elapsed_time = time.perf_counter() - t
 
+    # Display test information
     for file in files:
         if file[0].casefold() == 'failed':
             print(f'{Fore.RED}FAILED !! {file[1]} {Style.RESET_ALL}')
@@ -30,8 +51,10 @@ def generate_output(verbose=False):
     print(f'{Fore.GREEN}Passed {len(passed)} test(s)')
     print(f'{Fore.RED}Failed {len(failed)} test(s)')
 
+    # Calculate the coverage
     coverage = int(len(passed) / (len(passed) + len(failed)) * 100)
 
+    # Print it nice
     if coverage <= 50:
         print(f'{Fore.RED}', end='')
     elif 50 < coverage <= 75:
@@ -41,6 +64,10 @@ def generate_output(verbose=False):
     print(f'\nTotal Documentation Coverage {coverage}%', end=' ')
     print(f'{Style.RESET_ALL}\n')
 
+    # Generate a badge
+    generate_coverage_badge(coverage)
+
+    # Exit depending on failed tests, for CI
     if len(failed) > 0:
         sys.exit(-1)
     else:
